86741b3d224029c498f8b5d56e6f0d0a482772fa
fardog <nate@fardog.io>
2024-03-11 14:54:50 +0000

    update packages, fix sway for new desk layout

--- bin/swap-workspaces.sh
+++ bin/swap-workspaces.sh
@@ -4,8 +4,8 @@ workspaces=$(swaymsg -r -t get_workspaces)
 visible_workspace=$(echo ${workspaces} | jq -r '.[] | select(.visible == true and .focused == false) | .num')
 focused_workspace=$(echo ${workspaces} | jq -r '.[] | select(.focused == true) | .num')
 
-swaymsg move workspace to output up
+swaymsg move workspace to output right
 
 swaymsg workspace ${visible_workspace}
-swaymsg move workspace to output up
+swaymsg move workspace to output right
 
